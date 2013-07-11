9224557d707e9ea8940c04d0a82f7dd73dddc1d3
Louy <me@l0uy.com>
2013-07-11 04:39:07 +0400

    Creating plugin file

---
+++ featured-image-resize.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Featured Image Resize
+Plugin URI: http://wordpress.org/plugins/featured-image-resize/
+Description: This plugin will regenerate all missing image sizes for an image when it's chosen as a featured image for any post. Very useful when you change your template.
+Author: Louy Alakkad
+Version: 0.1
+Author URI: http://l0uy.com/
+*/
+

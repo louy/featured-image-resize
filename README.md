e30dfb2ec13f726817d1b5d0517b6bd0e655adf5
Louy <me@l0uy.com>
2013-07-11 05:34:06 +0400

    Removing debug code and changing version to 0.2. Updated description.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-featured-image-resize
+Featured Image Resize
 =====================
 
-This plugin will regenerate all missing image sizes for an image when it's chosen as a featured image for any post. Very useful when you change your template.
+This is a wordpress plugin that will regenerate all missing image sizes for an image when it's chosen as a featured image for any post. Very useful when you change your template.
+No settings whatsoever, just activate and forget.
--- featured-image-resize.php
+++ featured-image-resize.php
@@ -2,29 +2,25 @@
 /*
 Plugin Name: Featured Image Resize
 Plugin URI: http://wordpress.org/plugins/featured-image-resize/
-Description: This plugin will regenerate all missing image sizes for an image when it's chosen as a featured image for any post. Very useful when you change your template.
+Description: This plugin will regenerate all missing image sizes for an image when it's chosen as a featured image for any post. Very useful when you change your template. No settings whatsoever, just activate and forget.
 Author: Louy Alakkad
-Version: 0.1
+Version: 0.2
 Author URI: http://l0uy.com/
 */
 
-//add_action('wp_ajax_set-post-thumbnail-test','create_sizes_if_missing',1);
-add_action('wp_ajax_set-post-thumbnail','create_sizes_if_missing',1);
-function create_sizes_if_missing() {
+add_action('wp_ajax_set-post-thumbnail','featured_image_resize_if_missing',1);
+function featured_image_resize_if_missing() {
 	$thumbnail_id = $_REQUEST['thumbnail_id'];
 	if( !is_numeric($thumbnail_id) ) return false;
-	global $wpdb;
 	
 	$sizes = get_intermediate_image_sizes();
 	
 	$imagedata = wp_get_attachment_metadata( $thumbnail_id );
 	
-	/*echo '<pre>', print_r($imagedata,true), '</pre>';
-	if($_REQUEST['action'] == 'set-post-thumbnail-test') exit;*/
-	
 	if ( !is_array( $imagedata ) ) {
 		// image has no metadata, check for existence
 		
+		global $wpdb;
 		$post_exists = $wpdb->get_row("SELECT * FROM $wpdb->posts WHERE id = '" . $thumbnail_id . "'", 'ARRAY_A');
 		if( !$post_exists ) return false;
 		
@@ -50,5 +46,5 @@ function create_sizes_if_missing() {
 	$new = wp_generate_attachment_metadata( $thumbnail_id, $image_path );
 	wp_update_attachment_metadata( $thumbnail_id, $new );
 	
-	return;
+	return true;
 }
